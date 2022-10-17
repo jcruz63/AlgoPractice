9763d7cc59a59974803451e714ebef267cbeed77
jcruz63 <jonacruz63@gmail.com>
2022-10-16 21:38:30 -0400

    Completed removed duplicate

--- src/main/java/challenges/leetcode/RmoveDuplicatesLinkedList.java
+++ src/main/java/challenges/leetcode/RmoveDuplicatesLinkedList.java
@@ -1,7 +1,27 @@
 package challenges.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RmoveDuplicatesLinkedList {
     public static ListNode deleteDuplicates(ListNode head) {
+
+        Set<Integer> unique = new HashSet<>();
+
+        ListNode current = head;
+        ListNode previous = null;
+        int iteration = 1;
+
+        while (current != null){
+            if(unique.contains(current.val)){
+                previous.next = current.next;
+            } else {
+                unique.add(current.val);
+                previous = current;
+            }
+            current = current.next;
+
+        }
 
         return head;
     }
--- src/test/java/challenges/leetcode/RmoveDuplicatesLinkedListTest.java
+++ src/test/java/challenges/leetcode/RmoveDuplicatesLinkedListTest.java
@@ -10,6 +10,10 @@ class RmoveDuplicatesLinkedListTest {
     void deleteDuplicates() {
         int[] sample = {1, 1, 2};
         ListNode head = ListNode.listFromArray(sample);
+        int[] sampleWithRemoved = {1, 2};
+        ListNode test = ListNode.listFromArray(sampleWithRemoved);
+
+        assertEquals(test, RmoveDuplicatesLinkedList.deleteDuplicates(head));
 
     }
 }
